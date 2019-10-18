a1bcf1b2d7fdcebd082ff963f6a5206e98c9606b
Eric Rodriguez <rodriguez.eric.n@gmail.com>
2019-10-18 12:10:27 +1100

    final

--- cyk_parser.py
+++ cyk_parser.py
@@ -1,4 +1,4 @@
-from production_rule import ProductionRule
+from invalid_symbol_error import InvalidSymbolError
 
 class CYKParser():
 
@@ -6,13 +6,33 @@ class CYKParser():
         self._grammar = self.__parse_grammar(filepath)
 
     def parse(self, string):
+        """
+        Parses string
+
+        :param string: string to be parsed
+        :return: Boolean, true if string is recognised by the grammar, false otherwise
+        """
         assert type(string) is str, "invalid string passed to parse, not of type string"
 
         parse_table = self.__generate_parse_table(string)
 
         self.__print_parse_table(parse_table, string)
 
+        # string is recongised by grammar, print left derivation
+        if parse_table[::-1][0][0].get("S", False):
+            self.__print_left_derivation(parse_table, [parse_table[len(string) - 1][0]['S']])
+            return True
+        else:
+            return False
+
     def __generate_parse_table(self, string):
+        """
+        Generates CYK parse table
+
+        :param string: string to be parsed
+        :raises InvalidSymbolError: char in string is not a terminal in the grammar
+        :return: parse table
+        """
 
         # upper left triangular
         parse_table = [[{} for s in range(len(string) - l)] for l in range(len(string))]
@@ -20,23 +40,77 @@ class CYKParser():
         for i, char in enumerate(string):
             for production_rule in self._grammar:
                 if len(production_rule[1]) == 1 and production_rule[1][0] == char:
-                    parse_table[0][i][production_rule[0]] = True
+                    parse_table[0][i][production_rule[0]] = (1, production_rule, i, 1)
 
+        if any(map(lambda x : len(x) == 0, parse_table[0])):
+            raise InvalidSymbolError("ERROR_INVALID_SYMBOL")
+
+        # length of substring
         for l in range(2, len(string)+1):
-            # length of substring
+            # starting position of substring
             for s in range(0, len(string)-l+1):
-                # starting index of substring
+                # length of left division
                 for p in range(0, l-1):
                     for production_rule in self._grammar:
                         if len(production_rule[1]) == 2:
-                            b = parse_table[p][s].get(production_rule[1][0], False)
-                            c = parse_table[l - p - 2][s + p + 1].get(production_rule[1][1], False)
-                            if b and c:
-                                parse_table[l - 1][s][production_rule[0]] = True
-
+                            b = parse_table[p][s].get(production_rule[1][0], None)
+                            c = parse_table[l - p - 2][s + p + 1].get(production_rule[1][1], None)
+                            if not b is None and not c is None:
+                                parse_table[l - 1][s][production_rule[0]] = (p, production_rule, s, l)
         return parse_table
 
+    def __print_left_derivation(self, table, alpha):
+        """
+        Relies on the fact that there always exists a left most derivation. Recursively reduces the left most
+        production rule in alpha. Base case when no production rules remain, and alpha is printed a final time.
+        Left derivation printed is not unique.
+
+        :param table: cyk parse table
+        :param alpha: list containing start production rule
+        """
+
+        # find first production
+        prod = None
+        index = -1
+        for i, item in enumerate(alpha):
+            if isinstance(item, tuple):
+                prod = item
+                index = i
+                break
+
+        if prod is None:
+            return alpha
+
+        p = prod[0]
+        s = prod[2]
+        l = prod[3]
+
+        # prod is R -> terminal, replace with terminal
+        if l == 1:
+            terminal = prod[1][1][0]
+            alpha[index] = terminal
+        else:
+            # reduce Ra -> Rb, Rc
+            left_cell = table[p][s][prod[1][1][0]]
+            right_cell = table[l - p - 2][s + p + 1][prod[1][1][1]]
+            alpha = alpha[:index] + [left_cell, right_cell] + alpha[index + 1:]
+
+        # print formatted alpha
+        for item in alpha:
+            print(item[1][0], end=" ") if isinstance(item, tuple) else print(item, end=" ")
+        print()
+
+        self.__print_left_derivation(table, alpha)
+        return
+
     def __print_parse_table(self, table, string):
+        """
+        Prints CYK parse table to standard out. Cells contain Variable/s.
+
+        :param table:
+        :param string:
+        :return:
+        """
 
         table = table[::-1]
         for i in range(0, len(table)):
@@ -47,13 +121,16 @@ class CYKParser():
 
         for char in string:
             print('{}|| '.format(char.center(40)), end = " ")
+        print()
         return
 
 
     def __parse_grammar(self, filepath):
         """
-        Parses grammar from EBNF form to a Grammar object
-        :param filepath:
+        Parses grammar from EBNF form
+
+        :param filepath: file in grammar
+        :exception FileNotFoundError:
         :return Grammar object:
         """
         production_rules = []
@@ -63,11 +140,6 @@ class CYKParser():
                 line = line.replace(" ", "").replace(";", "").replace("\n", "").split("=")
 
                 if len(line) == 2:
-                    production_rules.extend([(line[0], body.split(",")) for body in line[1].split("|")])
+                    production_rules.extend([(line[0], tuple(body.split(","))) for body in line[1].split("|")])
 
-        # create instances
-        production_rule_objects = {}
-
-
-        print(production_rules)
         return production_rules
--- main.py
+++ main.py
@@ -1,16 +1,40 @@
+from sys import argv
 from cyk_parser import CYKParser
+from invalid_symbol_error import InvalidSymbolError
 
 def main():
-    cyk_parser = CYKParser("hello.txt")
-    #
-    # string = parse_string("./test_file_path.txt")
-    #
-    cyk_parser.parse("seaFwaf")
 
-    # cyk_parser.parse_grammar("hello.txt")
+    if len(argv) < 3:
+        print("Invalid number of arguments | requires file paths for grammar and string")
+        quit()
 
+    try:
+        string = open(argv[2]).read().replace('\n', '').replace(" ", "")
+    except FileNotFoundError as e:
+        print("Invalid file path for string, does not exist")
+        quit()
+    except Exception as e:
+        print(e.__repr__())
+        quit()
 
-    return
+    try:
+        cyk_parser = CYKParser(argv[1])
+    except FileNotFoundError as e:
+        print("Invalid file path for grammar, does not exist")
+        quit()
+    except Exception as e:
+        print(e.__repr__())
+        quit()
+
+    try:
+        print("ACCEPTED") if cyk_parser.parse(string) else print("REJECTED")
+    except InvalidSymbolError as e:
+        print(e.__str__())
+        quit()
+    except Exception as e:
+        print(e.__repr__())
+        quit()
+
 
 if __name__ == '__main__':
     main()
